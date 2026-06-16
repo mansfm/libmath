104ee559e79e557ec4f7af0389620389907ae452
Mikhail Mansurov <mansfm@yandex.ru>
2026-06-16 01:35:25 +0000

    clang-format

--- include/libmath.h
+++ include/libmath.h
@@ -7,4 +7,4 @@ int subtraction(int a, int b);
 int multiplication(int a, int b);
 int division(int a, int b, int& c);
 int power(int a, int b);
-}
+} // namespace libmath
--- src/libmath.cpp
+++ src/libmath.cpp
@@ -5,37 +5,37 @@ namespace libmath
 
 int addition(int a, int b)
 {
-	return a + b;
+    return a + b;
 }
 
 int subtraction(int a, int b)
 {
-	return a - b;
+    return a - b;
 }
 
 int multiplication(int a, int b)
 {
-	return a * b;
+    return a * b;
 }
 
 int division(int a, int b, int& c)
 {
-	if (b == 0)
-	{
-		return -1;
-	}
-	c = a / b;
-	return 0;
+    if (b == 0)
+    {
+        return -1;
+    }
+    c = a / b;
+    return 0;
 }
 
 int power(int a, int b)
 {
-	int c = 1;
-	for (int i = 0; i < b; ++i)
-	{
-		c = c * a;
-	}
-	return c;
+    int c = 1;
+    for (int i = 0; i < b; ++i)
+    {
+        c = c * a;
+    }
+    return c;
 }
 
-}
+} // namespace libmath
